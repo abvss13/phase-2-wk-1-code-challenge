574872727639e357ad40105d4e1d4cc834380d73
Your Name <abdullahi.abass@student.moringschool.com>
2023-07-24 17:54:17 +0300

    improved

---
+++ bank-of-flatiron/src/TransactionApp.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './index.css';
+import './App.css'; 
+
+const TransactionApp = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newTransaction, setNewTransaction] = useState({
+    description: '',
+    amount: '',
+    category: '',
+  });
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const fetchTransactions = async () => {
+    try {
+      const response = await axios.get('https://api.npoint.io/9a6438df7be8936787f7');
+      // If the transactions are nested inside an object, extract them here
+      const transactionsArray = response.data.transactions || response.data;
+
+      setTransactions(transactionsArray);
+    } catch (error) {
+      console.error('Error fetching transactions:', error);
+    }
+  };
+
+  const handleAddTransaction = () => {
+    // Add the newTransaction to the transactions state
+    setTransactions([...transactions, newTransaction]);
+
+    // Clear the form fields after adding a new transaction
+    setNewTransaction({ description: '', amount: '', category: '' });
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      {/* SearchBar component */}
+      <input
+        type="text"
+        placeholder="Search transactions..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {/* TransactionForm component */}
+      <div>
+        <input
+          type="text"
+          placeholder="Description"
+          value={newTransaction.description}
+          onChange={(e) =>
+            setNewTransaction({ ...newTransaction, description: e.target.value })
+          }
+        />
+        <input
+          type="number"
+          placeholder="Amount"
+          value={newTransaction.amount}
+          onChange={(e) => setNewTransaction({ ...newTransaction, amount: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Category"
+          value={newTransaction.category}
+          onChange={(e) => setNewTransaction({ ...newTransaction, category: e.target.value })}
+        />
+        <button onClick={handleAddTransaction}>Add Transaction</button>
+      </div>
+
+      {/* TransactionTable component */}
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredTransactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.description}</td>
+              <td>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TransactionApp;
